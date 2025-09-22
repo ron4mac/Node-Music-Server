26f03a23579d1c86fd0722dbcfb061754c43e66d
Ron Crans <ron@rjconline.net>
2025-09-22 12:08:21 -0400

    improve reliability of update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
-unzip newversion -x */lib/config.js -d update_folder
-cp -a update_folder/*/* ./
-rm -rf update_folder
-rm newversion.zip
+ZIPFILE="newversion.zip"
+EXCLUDE="*/lib/config.js"
+DFOLDER="update_folder"
+UPFILES="update_folder/ron4mac-Node-Music-Server-"
+TFOLDER="./"
+
+unzip "$ZIPFILE" -x "$EXCLUDE" -d "$DFOLDER"
+cp -a "$UPFILES"*/* "$TFOLDER"
+rm -rf "$DFOLDER"
+#rm "$ZIPFILE"
 npm install
