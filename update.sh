a84a9ca5e7ffc7bcd267ddde24c974930f2cd0a9
Ron Crans <ron@rjconline.net>
2025-09-26 09:48:33 -0400

    update with update.sh asset option

--- update.sh
+++ update.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/bash
 ZIPFILE="newversion.zip"
-EXCLUDE="*/lib/config.js"
-DFOLDER="update_folder"
-UPFILES="update_folder/ron4mac-Node-Music-Server-"
+UPFILES="ron4mac-Node-Music-Server"
 TFOLDER="./"
 
-unzip "$ZIPFILE" -x "$EXCLUDE" -d "$DFOLDER"
-cp -a "$UPFILES"*/* "$TFOLDER"
-rm -rf "$DFOLDER"
-#rm "$ZIPFILE"
+unzip -qq "$ZIPFILE" -x "*/update.sh" "*/lib/config.js" "*/lib/admin.js"
+D=$(pwd)
+for F in "$UPFILES"*; do
+    U="$F"
+done
+cp -a "${D}/${U}/." "${D}/upd/"
+if [ $? -ne 0 ]; then
+  echo "cp command failed!"
+  # Handle the error, e.g., exit or log the error
+fi
+rm -rf "$U"
+rm "$ZIPFILE"
 npm install
+echo "LIFE IS GOOD!"
